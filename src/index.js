1b5ca971d107608158cdb4a891da56ceb655a761
nebulasnft <165807106+nebulasnft@users.noreply.github.com>
2024-07-04 14:34:30 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDom.render(<App />,
-documnet.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
